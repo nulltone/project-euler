c6ae02cc36ff6767e9e806c585fa7b20179440bf
Randall Noriega <randallnoriega@gmail.com>
2015-02-19 00:15:01 -0800

    First attempt on solving problem 003

---
+++ problem_003/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut primes = vec![];
+    let maxNum = 600851475143;
+
+    for x in 2..maxNum {
+        println!("Foo!");
+        // if primes.as_slice().get(x) != None {
+        //     continue;
+        // } else {
+        //     primes[x] = true;
+        //     let mut inc = x;
+        //     while inc <= maxNum {
+        //         primes[inc] = false;
+        //         inc += inc;
+        //     }
+        // }
+        break;
+    }
+}
