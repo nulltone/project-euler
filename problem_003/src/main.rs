884f67ece133181263fe7875b79510577178c0ea
Randall Noriega <randallnoriega@gmail.com>
2015-03-01 18:33:11 -0800

    Used wrong algorithm, sieve of eratosthenes. Will work on simpler algorithm.

--- problem_003/src/main.rs
+++ problem_003/src/main.rs
@@ -3,31 +3,39 @@ use std::collections::HashMap;
 fn main() {
 	let mut primes : HashMap<u64, bool> = HashMap::new();
 	//let max_num: u64 = 600851475143;
-	let max_num: u64 = 13195;
+	let max_num: u64 = 35;
 
-	println!("Primes: ");
 	for x in 2..max_num {
+        // Checks if the value has been inserted already.
 		match primes.get(&x) {
-			Some(_) => {
+			Some(&exists) => {
+                // Value already inserted. Skipping.
+                println!("exists - primes[{}]:{}", x, exists);
 				continue;
 			} 
-			None => {}
+			None => {
+                // Inserts the prime if it doesn't exist.
+                primes.insert(x, true);
+                println!("inserted - primes[{}]:true", x);
+            }
 		}
 
-		primes.insert(x, true);
-		let mut inc = x;
+        // Marks off all multiples of current values to false.
+        let mut counter = 2;
+		let mut inc = x * counter;
 		while inc <= max_num {
 				primes.insert(inc, false);
-				inc += inc;
-		}
-
-		match primes.get(&x) {
-			Some(t) => {
-				if *t {
-					println!("{}", *t);
-				} 
-			}
-			None => {}
+                //println!("--- marked - primes[{}]:false", inc);
+                counter += 1;
+				inc = x * counter;
 		}
 	}
+
+    println!("Prime numbers: ");
+    for (&num, &is_prime) in primes.iter() {
+        if is_prime {
+            println!("{}", num);
+        }
+    }
 }
+
